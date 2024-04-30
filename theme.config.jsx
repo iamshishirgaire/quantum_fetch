b0c941484c7c0d883b9afec20ae6cab2c35294c2
iamshishirgaire <shishirgaire35@gmail.com>
2024-04-30 16:41:53 +0545

    Update logo component and footer text

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,13 +1,25 @@
-import Image from "next/image";
-
 export default {
   logo: (
     <>
-      <Image src={"/dart.svg"} height={30} width={30} alt="dart logo"></Image>
+      <img src={"/dart.svg"} height={30} width={30} alt="dart logo"></img>
       <span style={{ marginLeft: ".4em", fontWeight: 800 }}>Quantum Fetch</span>
     </>
   ),
   project: {
     link: "https://github.com/santoshacharya2468/flutter_quantum_fetch",
+  },
+  footer: {
+    text: (
+      <span>
+        BSD {new Date().getFullYear()} ©{" "}
+        <a
+          href="https://github.com/santoshacharya2468/flutter_quantum_fetch"
+          target="_blank"
+        >
+          Quantum Fetch
+        </a>
+        .
+      </span>
+    ),
   },
 };
